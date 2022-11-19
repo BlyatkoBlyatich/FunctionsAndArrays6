8238929560107534a015bc8c94f630ae6305bd0d
Василий Виссаров <vissar2016@rambler.ru>
2022-11-19 03:35:45 +0300

    A program has been added, a program that searches for the coordinates of two lines given by equations  y = k1 * x + b1, y = k2 * x + b2. The output of the program shows at what point the lines intersect and whether they are parallel.

---
+++ BiggerThenZero/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.*/
+
+int BiggerThenZero(int size)
+{
+    int [] array = new int [size];
+    int count = 0;
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine());
+        
+        if(array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.WriteLine("Input size of array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Numbers bigger then zero in array is: {BiggerThenZero(size)}");
